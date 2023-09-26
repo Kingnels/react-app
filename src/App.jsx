5790dd38116be2c3e341a2908a17b3a17dcdb2a1
Kingnels <ifeanyiibe2022@gmail.com>
2023-09-27 00:20:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+ 
+    <div className="div">
+      thid is my first vite site
+    </div>
+     
+  )
+}
+
+export default App
